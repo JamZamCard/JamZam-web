c6b3af8a6d208e05a4b2d08ef01df8c06b3085aa
David A <d.ananda974@gmail.com>
2022-01-14 21:34:50 +0100

    dark mode and full tailwind config

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="flex justify-center">
     <div class="container ">
-      <div class="bg-gray-100 rounded-xl p-8 dark:bg-gray-800">
-        <p>Welcome in TODO: host name</p>
+      <div class="bg-gray-800 rounded-xl p-8 ">
+        <p class="text-white">Welcome in TODO: host name</p>
         <div class="bg-black-100 rounded-xl ">
           <ul class="bg-white"></ul>
         </div>
+        <div class="mt-5">
         <input type="text" name="message"
                class="mt-1 px-3 py-2 bg-white border shadow-sm border-gray-300
                   placeholder-gray-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500
                   block w-full rounded-md sm:text-sm focus:ring-1"
                placeholder="Message" v-model="msg"/>
-        <button class="dark:md:hover:bg-fuchsia-600" v-on:click="send">
-          Send
-        </button>
+        </div>
+        <div class="mt-5">
+          <button class="hover:bg-fuchsia-600 border-2
+        border-green-500 bg-green-400 hover:bg-green-600 rounded-xl
+        w-max py-2 px-3" v-on:click="send">
+            Send
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -38,6 +44,7 @@ export default {
       this.socket.on('msgFromServerToClient', (text) => {
         const el = document.createElement('li');
         el.innerHTML = text;
+        el.className = 'text-white';
         document.querySelector('ul')
           .appendChild(el);
       });
